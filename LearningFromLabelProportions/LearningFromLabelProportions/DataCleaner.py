a4ffec3a87541754a4c7a9d10456bcec351835fa
Hadi Saleh <hadi.saleh852@outlook.com>
2020-02-24 12:25:54 +0100

    chenged 2 and 7 to 0 and 1

--- LearningFromLabelProportions/LearningFromLabelProportions/DataCleaner.py
+++ LearningFromLabelProportions/LearningFromLabelProportions/DataCleaner.py
@@ -21,16 +21,16 @@ def get_processed_mnist_data(group_size):
 
     # Separate zeroes and ones
 
-    zeroes_train = np.array([x_train[i] for i in range(len(x_train)) if y_train[i] == 2])
-    ones_train = np.array([x_train[i] for i in range(len(x_train)) if y_train[i] == 7])
+    zeroes_train = np.array([x_train[i] for i in range(len(x_train)) if y_train[i] == 0])
+    ones_train = np.array([x_train[i] for i in range(len(x_train)) if y_train[i] == 1])
 
-    zeroes_test = np.array([x_test[i] for i in range(len(x_test)) if y_test[i] == 2])
-    ones_test = np.array([x_test[i] for i in range(len(x_test)) if y_test[i] == 7])
+    zeroes_test = np.array([x_test[i] for i in range(len(x_test)) if y_test[i] == 0])
+    ones_test = np.array([x_test[i] for i in range(len(x_test)) if y_test[i] == 1])
 
     # The labels are an array of shuffled 0's and 1's
     
-    shuffled_labels_train = np.array([2 for i in range(len(zeroes_train))] + [7 for i in range(len(ones_train))])
-    shuffled_labels_test = np.array([2 for i in range(len(zeroes_test))] + [7 for i in range(len(ones_test))])
+    shuffled_labels_train = np.array([0 for i in range(len(zeroes_train))] + [1 for i in range(len(ones_train))])
+    shuffled_labels_test = np.array([0 for i in range(len(zeroes_test))] + [1 for i in range(len(ones_test))])
     np.random.shuffle(shuffled_labels_train)
     np.random.shuffle(shuffled_labels_test)
 
@@ -41,7 +41,7 @@ def get_processed_mnist_data(group_size):
     i_one = 0
 
     for i in shuffled_labels_train:
-        if i == 2:
+        if i == 0:
             shuffled_data_train.append(zeroes_train[i_zero])
             i_zero += 1
         else:
@@ -55,7 +55,7 @@ def get_processed_mnist_data(group_size):
     i_one = 0
 
     for i in shuffled_labels_test:
-        if i == 2:
+        if i == 0:
             shuffled_data_test.append(zeroes_test[i_zero])
             i_zero += 1
         else:
@@ -71,8 +71,8 @@ def get_processed_mnist_data(group_size):
                 np.array([sum([1 for l in labels[i:i + group_size] if l == measured_label]) / group_size for i in range(0, len(data), group_size) if i + group_size <= len(data)]))
 
     #dim_1, dim_2 = 5, 4
-    final_data_train, final_labels_train = group(shuffled_data_train, shuffled_labels_train, group_size, 7)
-    final_data_test, final_labels_test = group(shuffled_data_test, shuffled_labels_test, group_size, 7)
+    final_data_train, final_labels_train = group(shuffled_data_train, shuffled_labels_train, group_size, 1)
+    final_data_test, final_labels_test = group(shuffled_data_test, shuffled_labels_test, group_size, 1)
     
     # final_data shape: (groupCount, groupSize, 28: width, 28: height, 1) each element contains a pixel value between 0 and 1
     # final_labels shape: (groupCount,) each element contains a value between 0 and 1, the proportion of 1's
